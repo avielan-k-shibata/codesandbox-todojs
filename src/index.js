33970cc12be6e31ba0ea8fcaaf96b8cff1110407
avielan-k-shibata <67250245+avielan-k-shibata@users.noreply.github.com>
2022-07-04 17:41:58 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+`;
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得して初期化する。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li, div生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+
+  p.innerText = inputText;
+
+  //button(完了)
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+  //button(削除)
+  const deleteButton = document.createElement("button");
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+  deleteButton.innerText = "削除";
+  //liの下に各要素を格納
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了リストに追加
+  document.getElementById("imcomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
